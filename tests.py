707c934171e8cea5b56cec443de2a0ab9829e7cb
tejas.krishnan81 <tejas.krishnan81@gmail.com>
2025-04-09 09:19:36 -0400

    fix topics for functional analysis 1+2 and information theory.

--- tests.py
+++ tests.py
@@ -1,31 +1,28 @@
-from pdfminer.high_level import extract_text
-from json_repair import repair_json
 import json
 import os
 
 chapters = [
-    "Introduction",
-    "Measurable sets and functions",
-    "Measures",
-    "Independence",
-    "Integration",
-    "Further results on integration",
-    "Conditional expectation",
-    "Filtrations and stopping times",
-    "Martingales in discrete time",
-    "Applications of martingale theory",
+    "Entropy, Divergence, and Mutual Information",
+    "Codes and sequences",
+    "Optimal Codes and Shannon's first theorem",
+    "Channel Coding: Shannon's second theorem",
+    "Noisy Channels with non-iid input",
 ]
 
 with open(
-    os.path.join("saved_course_jsons", "probability_measure_martingales.json"),
+    os.path.join("saved_course_jsons", "information_theory.json"),
     "r",
     encoding="utf-8",
 ) as file:
     theorems = json.load(file)
 
 for i, theorem in enumerate(theorems):
-    topic_num = int(theorem["id"].split()[1][0])
-    theorems[i]["topic"] = chapters[topic_num]
+    try:
+        topic_num = int(theorem["id"].split()[1][0])-1
+        theorems[i]["topic"] = chapters[topic_num]
+    except Exception as e:
+        print(f"Error processing theorem {i}: {theorem['id']}")
+        print(e)
 
-with open(os.path.join("saved_course_jsons", "pmm_edited.json"), "w+") as f:
+with open(os.path.join("saved_course_jsons", "information_theory_edited.json"), "w+") as f:
     json.dump(theorems, f, indent=4)
